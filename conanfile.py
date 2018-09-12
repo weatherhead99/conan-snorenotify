66c50b784229ba0769bbc0f5959770d68a4187bb
Dan Weatherill <plasteredparrot@gmail.com>
2018-09-12 05:26:36 +0100

    bump version

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ import os
 
 class SnorenotifyConan(ConanFile):
     name = "snorenotify"
-    version = "0.5.3"
+    version = "0.7.0"
     license = "LGPL-v3"
     url = "https://github.com/weatherhead99/conan-snorenotify"
     description = "Snorenotify is a multi platform Qt notification framework."
@@ -15,8 +15,8 @@ class SnorenotifyConan(ConanFile):
     exports_sources = "build_with_conan.patch"
     requires="Qt/5.11.1@bincrafters/stable"
     build_requires="extra-cmake-modules/5.50.0@weatherhead99/testing"
-    sha256 = "7d9b13f8994ac18c08ec7e5dcf5bd335e11a4f6203d61ab0765c9e4085b11e7e"
-
+    sha256 = "2e3f5fbb80ab993f6149136cd9a14c2de66f48cabce550dead167a9448f5bed9"
+    
     def config_options(self):
         if self.settings.os != "Linux":
             del self.options.freedesktop
@@ -28,7 +28,7 @@ class SnorenotifyConan(ConanFile):
         tools.get("https://github.com/KDE/%s/archive/v%s.tar.gz"
                   % (self.name,self.version), sha256=self.sha256)
         tools.patch(patch_file="build_with_conan.patch")
-        
+
     def build(self):
         cmake = CMake(self)
         sf = os.path.join(self.source_folder,"snorenotify-%s" % self.version)
