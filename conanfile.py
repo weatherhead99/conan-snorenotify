39a0937326f6ffd90f4cbaa7cb9386188fd1729c
Dan Weatherill <plasteredparrot@gmail.com>
2018-09-12 04:05:24 +0100

    added suitable requirements

--- conanfile.py
+++ conanfile.py
@@ -11,13 +11,16 @@ class SnorenotifyConan(ConanFile):
     options = {"shared": [True, False]}
     default_options = "shared=False"
     generators = "cmake"
-    requires="Qt/5.11.1@bincrafters/stable"
+    exports_sources = "build_with_conan.patch"
+    requires="Qt/5.11.1@bincrafters/stable", \
+              "extra-cmake-modules/5.50.0@weatherhead99/testing"
     sha256 = "7d9b13f8994ac18c08ec7e5dcf5bd335e11a4f6203d61ab0765c9e4085b11e7e"
     
     def source(self):
         tools.get("https://github.com/KDE/%s/archive/v%s.tar.gz"
                   % (self.name,self.version), sha256=self.sha256)
-
+        tools.patch(patch_file="build_with_conan.patch")
+        
     def build(self):
         cmake = CMake(self)
         sf = os.path.join(self.source_folder,"snorenotify-%s" % self.version)
@@ -25,15 +28,7 @@ class SnorenotifyConan(ConanFile):
         cmake.configure(source_folder=sf)
         cmake.build()
 
-
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        pass
 
-    def package_info(self):
-        self.cpp_info.libs = ["hello"]
 
